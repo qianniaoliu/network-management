f7d298b68b91ce212fbb495670cc3029c0bbbe4c
shenlong7 <cdshenlong1@jd.com>
2022-09-13 23:10:11 +0800

    [优化代码]

--- network-management-websocket/src/main/java/com/network/management/websocket/EquipmentStatusSynchronizationEngine.java
+++ network-management-websocket/src/main/java/com/network/management/websocket/EquipmentStatusSynchronizationEngine.java
@@ -62,23 +62,24 @@ public class EquipmentStatusSynchronizationEngine implements InitializingBean {
     public void start() {
         SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
         scheduledExecutorService.scheduleAtFixedRate(() -> {
-            log.info("基站状态推送定时任务开始执行时间：{}", dateFormat.format(new Date()));
-            BordInformationAggregation bordInformationAggregation = bordInformationService.getAll();
-            if (CollectionUtils.isNotEmpty(bordInformationAggregation.getEquipments())) {
-                CountDownLatch countDownLatch = new CountDownLatch(bordInformationAggregation.getEquipments().size());
-                bordInformationAggregation.getEquipments().forEach(equipment -> {
-                    equipmentStatusQueryExecutorService.execute(() -> doQueryEquipmentStatus(countDownLatch, equipment));
-                });
-                try {
+            try {
+                log.info("基站状态推送定时任务开始执行时间：{}", dateFormat.format(new Date()));
+                BordInformationAggregation bordInformationAggregation = bordInformationService.getAll();
+                if (CollectionUtils.isNotEmpty(bordInformationAggregation.getEquipments())) {
+                    CountDownLatch countDownLatch = new CountDownLatch(bordInformationAggregation.getEquipments().size());
+                    bordInformationAggregation.getEquipments().forEach(equipment -> {
+                        equipmentStatusQueryExecutorService.execute(() -> doQueryEquipmentStatus(countDownLatch, equipment));
+                    });
                     countDownLatch.await();
-                } catch (InterruptedException e) {
+                    log.info("基站状态推送定时任务执行结束时间：{}", dateFormat.format(new Date()));
+                    if (!equipmentStatusCombinationMapping.isEmpty()) {
+                        log.info("当前基站状态信息推送个数：{}", equipmentStatusCombinationMapping.size());
+                        //当所有基站信息都查询完成后，基站信息推送给前端进行展示
+                        publisher.publish(new EquipmentStatusEvent(equipmentStatusCombinationMapping.values()));
+                    }
                 }
-                log.info("基站状态推送定时任务执行结束时间：{}", dateFormat.format(new Date()));
-                if (!equipmentStatusCombinationMapping.isEmpty()) {
-                    log.info("当前基站状态信息推送个数：{}", equipmentStatusCombinationMapping.size());
-                    //当所有基站信息都查询完成后，基站信息推送给前端进行展示
-                    publisher.publish(new EquipmentStatusEvent(equipmentStatusCombinationMapping.values()));
-                }
+            } catch (Exception ex) {
+                log.error("基站状态推送定时任务处理失败!", ex);
             }
         }, 1, 60, TimeUnit.SECONDS);
     }
@@ -97,16 +98,6 @@ public class EquipmentStatusSynchronizationEngine implements InitializingBean {
             EquipmentStatusCombination equipmentStatusCombination = new EquipmentStatusCombination(equipmentId, deviceStatusVo,
                     powerStatusVo);
             equipmentStatusCombinationMapping.put(equipment.getId(), equipmentStatusCombination);
-            //if (equipmentStatusCombinationMapping.containsKey(equipment.getId())) {
-            //    EquipmentStatusCombination existEquipmentStatusCombination = equipmentStatusCombinationMapping.get(equipmentId);
-            //    if (Objects.equals(equipmentStatusCombination, existEquipmentStatusCombination)) {
-            //        equipmentStatusCombinationMapping.remove(equipmentId);
-            //    } else {
-            //        equipmentStatusCombinationMapping.put(equipment.getId(), equipmentStatusCombination);
-            //    }
-            //} else {
-            //    equipmentStatusCombinationMapping.put(equipment.getId(), equipmentStatusCombination);
-            //}
         } catch (Exception ex) {
             log.error("定时任务查询基站状态失败,基站id：{}", equipment.getId());
         } finally {
